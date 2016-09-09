bdf1221377cc4ced7ae32bd0f39b554108c02473
Mark Ioannidis <mark.ioannidis@gmail.com>
2016-09-09 11:16:30 -0400

    spacing update

--- numguess.rb
+++ numguess.rb
@@ -3,19 +3,19 @@ ui= gets.to_i
 rn= rand(9)+1 # to get 1-10
 
 for i in 1..3
-		if ui==rn
-			puts "You guessed correctly"
-			break
-		elsif i<3
-			puts"Guess Again"
-			ng=gets.to_i
-			if ng==rn
-				puts "You guessed right!"
-			else
-				puts "You guessed wrong"
-			end
-		else 
-			puts "Your third guess was incorrect. Sorry!"
-			puts "The correct number was #{i}"
+	if ui==rn
+		puts "You guessed correctly"
+		break
+	elsif i<3
+		puts"Guess Again"
+		ng=gets.to_i
+		if ng==rn
+			puts "You guessed right!"
+		else
+			puts "You guessed wrong"
 		end
+	else 
+		puts "Your third guess was incorrect. Sorry!"
+		puts "The correct number was #{i}"
+	end
 end
